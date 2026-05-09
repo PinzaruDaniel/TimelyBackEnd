6882d267a96be3fb4b2673808031d4bb1e751a40
Pînzaru Daniel <pinzaru.danieel@gmail.com>
2026-05-09 13:28:21 +0300

    Added Jwt Key

---
+++ Migrations/20251208185034_AddImageUrlToHomework.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimelyBackEnd.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageUrlToHomework : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Homeworks",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Homeworks");
+        }
+    }
+}
--- obj/Debug/net9.0/TimelyBackEnd.AssemblyInfo.cs
+++ obj/Debug/net9.0/TimelyBackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimelyBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad86d4a0ba9f06f1b41f3cafbda97f878730cfb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64412d0182993c7d8167dba379c6840121750b6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimelyBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimelyBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
